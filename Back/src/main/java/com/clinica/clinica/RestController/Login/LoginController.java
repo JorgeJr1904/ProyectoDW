ab47fff1d819d52d668141814af375c91a70d1c2
JorgeJr <6wxks0nt8uehx1290wkhxe7xukenrs@bots.bitbucket.org>
2023-11-01 23:33:51 -0600

    CDW-13 Creacion de los endpoint para el login y creacion de usuarios

--- Back/src/main/java/com/clinica/clinica/RestController/Login/LoginController.java
+++ Back/src/main/java/com/clinica/clinica/RestController/Login/LoginController.java
@@ -1,13 +1,12 @@
 package com.clinica.clinica.RestController.Login;
 
 import com.clinica.clinica.Models.Dao.Login.LoginDao;
+import com.clinica.clinica.Models.Dao.ValidTokenDao;
 import com.clinica.clinica.Models.Entities.Login.User;
 import com.clinica.clinica.Response.AuthMessage;
+import com.clinica.clinica.Response.Message;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/login")
@@ -16,9 +15,17 @@ public class LoginController {
     @Autowired
     LoginDao loginDao;
 
+    @Autowired
+    ValidTokenDao validTokenDao;
+
     @GetMapping()
     public AuthMessage authLogin(@RequestBody User user){
         return loginDao.loginAuth(user.getUsername(), user.getPassword());
+    }
+
+    @PostMapping("auth")
+    public boolean authPromise(@RequestHeader(value = "Authorization") String token){
+        return validTokenDao.validToken(token);
     }
 
 }
